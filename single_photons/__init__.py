c879e2adfb1da50dcc03a502c1fe8fba28f807ad
oscarkremer <oscar.s.kremer@hotmail.com>
2023-02-14 18:35:22 -0300

    feat: copy script from lyapunov for ricatti

--- single_photons/__init__.py
+++ single_photons/__init__.py
@@ -2,12 +2,13 @@
 from .gaussian_dynamics import gaussian_dynamics
 from .gaussian_state import gaussian_state
 from .hermite_multidimensional import Hermite_multidimensional, Hermite_multidimensional_original
-from .lyapunov import lyapunov_ode_unconditional, lyapunov_ode_unconditional
-
+from .lyapunov import lyapunov_ode_unconditional, lyapunov_ode_conditional
+from .ricatti import ricatti_ode_unconditional, ricatti_ode_conditional
 
 __all__ = [
     'gaussian_dynamics',
     'gaussian_state',
     'hermite_multidimensional'
-    'lyapunov'
+    'lyapunov',
+    'ricatti'
 ]
---
+++ single_photons/ricatti.py
@@ -0,0 +1,48 @@
+import numpy as np
+from numpy.linalg import det
+from numpy.linalg import matrix_power
+from scipy.integrate import solve_ivp
+from scipy.linalg import solve_continuous_lyapunov
+from scipy.linalg import block_diag
+from scipy.linalg import sqrtm
+from scipy.linalg import fractional_matrix_power
+
+
+def ricatti_ode_unconditional(t, V_old_vector, A, D):
+    """
+    Auxiliar internal function defining the Lyapunov equation 
+    and calculating the derivative of the covariance matrix
+    """
+    
+    M = A.shape[0];                                                             # System dimension (N_particles + 1 cavity field)partículas  + 1 campo)
+    
+    A_T = np.transpose(A)                                                       # Transpose of the drift matrix
+    
+    V_old = np.reshape(V_old_vector, (M, M));                                      # Vector -> matrix
+    
+    dVdt = np.matmul(A, V_old) + np.matmul(V_old, A_T) + D;                     # Calculate how much the CM derivative in this time step
+    
+    dVdt_vector = np.reshape(dVdt, (M**2,));                                     # Matrix -> vector
+    return dVdt_vector
+
+def ricatti_ode_conditional(t, V_old_vector, A, D, B):
+    """
+    Auxiliar internal function defining the Lyapunov equation 
+    and calculating the derivative of the covariance matrix
+    """
+    
+    M = A.shape[0];                                                             # System dimension (N_particles + 1 cavity field)partículas  + 1 campo)
+    
+    A_T = np.transpose(A)                                                       # Transpose of the drift matrix
+    
+    V_old = np.reshape(V_old_vector, (M, M));                                   # Vector -> matrix
+    
+    # chi = np.matmul(C, V_old) + Gamma             # THIS SHOULD BE FASTER!
+    # chi = np.matmul(np.transpose(chi), chi)
+    # chi = np.matmul( np.matmul(V_old, np.transpose(C)) + np.transpose(Gamma),  np.matmul(C, V_old) + Gamma )    # Auxiliar matrix
+    chi = np.matmul( np.matmul( np.matmul(V_old, B), np.transpose(B)), V_old)    # Auxiliar matrix
+    
+    dVdt = np.matmul(A, V_old) + np.matmul(V_old, A_T) + D - chi;               # Calculate how much the CM derivative in this time step
+    
+    dVdt_vector = np.reshape(dVdt, (M**2,));                                    # Matrix -> vector
+    return dVdt_vector
