57c26e23d6d95f617f44843d468ccd06effae42c
igorclfrr <califrerjoseigor@gmail.com>
2023-06-06 23:40:44 -0300

    Update 4. Simulation Kalman Cavity_Particle.py
    implemented physical parameters related to back action. also tweaked plot to omit first estimation step

--- notebooks/4. Simulation Kalman Cavity_Particle.py
+++ notebooks/4. Simulation Kalman Cavity_Particle.py
@@ -15,12 +15,13 @@ from single_photons.environment import Cavity_Particle
 
 # In[2]:
 
-T = 293.15
+T = 293
 m_gas = ct.amu * (0.01 * 40 + 0.78 * 28 + 0.21 * 32)
 v_gas = np.sqrt(3 * ct.kb * T / m_gas)
 p = 1e-5
 R = 147e-9
 rho = 2200
+Nm = rho/(ct.amu*60.08) #SiO2 molecular density
 m_p = 4 / 3 * np.pi * R**3 * rho
 tweezer_wavelength = 1.55e-6
 tweezer_freq = 2 * np.pi * ct.c / tweezer_wavelength
@@ -29,8 +30,8 @@ tweezer_power = 200e-3
 tweezer_waist = 0.6e-6
 cavity_waist = 100e-6
 cavity_length = 50e-3
-coupling = 0.01
-eta_detec = 0.9
+eta_detec = 0.178
+pol_permit_ratio = 3/Nm*(index_refraction**-1)/(index_refraction**2+2) #from C-M
 
 gamma = 15.8 * R**2 * p / (m_p * v_gas)
 omega = (
@@ -39,8 +40,11 @@ omega = (
     * np.sqrt(tweezer_power)
     / (tweezer_waist**2 * np.sqrt(rho * ct.c))
 )
-detuning = 0.3 * omega
-cavity_linewidth = omega
+coupling = 9*pol_permit_ratio**2*tweezer_power*tweezer_freq**5/\
+    (128*np.pi**2*ct.c**6*m_p*omega)
+
+detuning = 1 * omega
+cavity_linewidth = 3 * omega
 
 cavity_freq = detuning + tweezer_freq
 g_cs = (
@@ -50,7 +54,8 @@ g_cs = (
     / (np.sqrt(cavity_length) * cavity_waist)
 )
 
-g_fb = 1e5
+g_fb = 1e6
+
 period = 2 * np.pi / omega
 t = np.arange(0, 10 * period, period / 1000)
 N = t.shape[0]
@@ -71,7 +76,7 @@ env = Cavity_Particle(
     eta_detection=eta_detec,
 )
 
-pulse_amplitude = 1e3
+pulse_amplitude = 900
 pulse_center = 2500
 pulse_width = 200
 alpha_in = []
@@ -111,9 +116,9 @@ Ad = scipy.linalg.expm(env.A * delta_t)
 # In[60]:
 
 
-P0 = 0.5 * np.matrix(np.eye(4))
+P0 = 1*np.matrix(np.eye(4))
 estimation = np.matrix([[0], [0], [0], [0]])
-states = np.array([[0], [0], [1.0], [0.0]])
+states = np.array([[0], [0], [5.0], [0.0]])
 K = np.array([[0, 0, 1, 1e5]])
 new_states = np.zeros((N, 4))
 measured_states = np.zeros((N))
@@ -129,7 +134,7 @@ for i in tqdm(range(t.shape[0])):
     kalman.compute_aposteriori(measured_states[i])
     estimated_states[i, :] = kalman.estimates_aposteriori[i][:, 0].reshape((4))
     estimation = estimated_states[i, :].reshape((4, 1))
-    control = -g_fb * estimation[2]
+    control = -g_fb * estimation[3]
     states = env.step(states, alpha_in=alpha_in[i], control=control, delta_t=delta_t)
 
 
@@ -137,9 +142,9 @@ for i in tqdm(range(t.shape[0])):
 plt.close("all")
 fig1 = plt.Figure()
 plt.title("Position")
-plt.plot(t[:], measured_states[:])
-plt.plot(t[:], estimated_states[:, 2])
-plt.plot(t[:], new_states[:, 2])
+plt.plot(t[1:], measured_states[1:])
+plt.plot(t[1:], estimated_states[1:, 2])
+plt.plot(t[1:], new_states[1:, 2])
 plt.grid()
 plt.legend(["Measured", "Estimated", "Simulated"])
 plt.show()
@@ -160,8 +165,8 @@ plt.show()
 fig2 = plt.Figure()
 plt.figure()
 plt.title("Photon number")
-plt.plot(t, np.power(estimated_states[:, 0], 2) + np.power(estimated_states[:, 1], 2))
-plt.plot(t, np.power(new_states[:, 0], 2) + np.power(new_states[:, 1], 2))
+plt.plot(t[1:], np.power(estimated_states[1:, 0], 2) + np.power(estimated_states[1:, 1], 2))
+plt.plot(t[1:], np.power(new_states[1:, 0], 2) + np.power(new_states[1:, 1], 2))
 plt.grid()
 plt.legend(["Estimated", "Simulated"])
 plt.show()
